09ef10b720231b4f7ef8f6e1f7be8755d3aec5e4
Renato Lui Geh <renatogeh@gmail.com>
2019-02-17 17:44:38 -0300

    Add pushables and updating to Timer
    Signed-off-by: Renato Lui Geh <renatogeh@gmail.com>

--- include/timer.hh
+++ include/timer.hh
@@ -4,29 +4,50 @@
 #include <thread>
 #include <chrono>
 #include <vector>
+#include <mutex>
 
 #include "updateable.hh"
 #include "bot.hh"
 
-template<class Rep, class Period> class Timer {
+#define TIMER_DEF_REFRESH_RATE std::chrono::milliseconds(100)
+
+class Timer {
   public:
-    Timer(Bot *bot, const std::chrono::duration<Rep, Period> &d) : b(bot), delta(d) {}
+    // If default constructor is used, Timer::Initialized must be called!
+    Timer(void) : delta(0) {};
+    template<class R1, class P1, class R2, class P2> Timer(Bot *bot,
+        const std::chrono::duration<R1, P1> &d,
+        const std::chrono::duration<R2, P2> &r = TIMER_DEF_REFRESH_RATE);
     ~Timer(void) {}
+
+    template<class R1, class P1, class R2, class P2> void Initialize(Bot *bot,
+        const std::chrono::duration<R1, P1> &d,
+        const std::chrono::duration<R2, P2> &r = TIMER_DEF_REFRESH_RATE) {
+      b = bot;
+      delta = std::chrono::duration_cast<decltype(delta)>(d);
+      refresh = std::chrono::duration_cast<decltype(refresh)>(r);
+      initialized = true;
+      running = false;
+    }
 
     void Start(void);
     void Stop(void);
 
+    void Push(Updateable *u);
+
     bool Running(void) { return running; }
 
   private:
-    bool running;
+    bool initialized, running;
     Bot *b;
+    std::mutex mu_r, mu_u;
     std::thread t;
-    const std::chrono::duration<Rep, Period> delta;
+    std::chrono::milliseconds delta, refresh;
     std::vector<Updateable*> ups;
 
-    static void f(Bot *b, Timer<Rep, Period> *self, const std::chrono::duration<Rep, Period> &delta,
-        std::vector<Updateable*> ups);
+    static void f(Bot *b, Timer *self);
 };
+
+#undef TIMER_DEF_REFRESH_RATE
 
 #endif
--- src/timer.cc
+++ src/timer.cc
@@ -1,28 +1,64 @@
+#include <cstdio>
+
 #include <thread>
 #include <chrono>
 #include <vector>
 
 #include "updateable.hh"
+#include "utils.hh"
 
 #include "timer.hh"
 
-template<class Rep, class Period> void Timer<Rep, Period>::Start(void) {
+#define TIMER_REFRESH_RATE 1s
+
+template<class R1, class P1, class R2, class P2> Timer::Timer(Bot *bot,
+    const std::chrono::duration<R1, P1> &d,
+    const std::chrono::duration<R2, P2> &r) { Initialize(bot, d, r); }
+
+void Timer::Start(void) {
+  if (!initialized) {
+    utils::PrintError("Attempted to run uninitialized Timer!");
+    return;
+  } else if (running) return;
   running = true;
-  t = std::thread(Timer::f, b, this, delta, ups);
+  t = std::thread(Timer::f, b, this);
 }
 
-template<class Rep, class Period> void Timer<Rep, Period>::Stop(void) {
+void Timer::Stop(void) {
+  if (!running) return;
+  mu_r.lock();
   running = false;
+  mu_r.unlock();
   t.join();
 }
 
-template<class Rep, class Period> void Timer<Rep, Period>::f(Bot *b, Timer<Rep, Period> *self,
-    const std::chrono::duration<Rep, Period> &delta, std::vector<Updateable*> ups) {
+void Timer::Push(Updateable *u) {
+  mu_u.lock();
+  ups.push_back(u);
+  mu_u.unlock();
+}
+
+void Timer::f(Bot *b, Timer *self) {
+  using namespace std::chrono_literals;
+  std::chrono::milliseconds elapsed = self->delta;
   while (1) {
-    if (!self->running) return;
-    std::this_thread::sleep_for(delta);
-    for (auto it = ups.begin(); it != ups.end(); ++it)
-      (*it)->Update((void*) b);
+    self->mu_r.lock();
+    if (!self->running) {
+      self->mu_r.unlock();
+      return;
+    }
+    self->mu_r.unlock();
+
+    std::this_thread::sleep_for(self->refresh);
+    elapsed -= self->refresh;
+    if (elapsed <= 0s) {
+      puts("Updating GitLab...");
+      self->mu_u.lock();
+      for (auto it = self->ups.begin(); it != self->ups.end(); ++it)
+        (*it)->Update((void*) b);
+      self->mu_u.unlock();
+      elapsed = self->delta;
+    }
   }
 }
 
